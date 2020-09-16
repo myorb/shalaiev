3fc2e37f595750608e8158bb931d5e3780a125f4
alex <itdep@gmail.com>
2020-09-17 01:04:53 +0200

    fix icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: process.env.NODE_ENV === 'production' ? '/shalaiev/favicon/favicon.ico' : '/favicon/favicon.ico' }
     ]
   },
   /*
@@ -77,6 +77,6 @@ export default {
   build: {
   },
   router: {
-    base: process.env.NODE_ENV === 'production' ? '/shalaiev/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/shalaiev/' : '/'
   }
 }
