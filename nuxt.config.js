5b4dfd6c498f496df250ee6124b5b0d93ed9a39f
alex <itdep@gmail.com>
2020-09-17 00:50:13 +0200

    fix url2

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
   */
   build: {
   },
-  env: {
-    baseUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://myorb.github.io/shalaiev'
+  router: {
+    base: process.env.NODE_ENV === 'production' ? '/shalaiev/' : ''
   }
 }
