2db2509ed4f9d0b4cdcbef1f1670f24ca243af06
myorb <myorb@users.noreply.github.com>
2020-09-23 16:50:43 +0000

    deploy: 2fceee4f2d2bf9041f0d9594f7ae72d8c38c0f41

---
+++ _nuxt/static/1600879842/articles/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1600879842",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"d520e324"}}}}("\u002Farticles"));
---
+++ _nuxt/static/1600879842/ll/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/ll", {data:[{}],fetch:[],mutations:void 0});
